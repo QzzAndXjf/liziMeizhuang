384b5c409baf61667d2d72a5248e8d2d86fb7add
Jfler <44290034+Jfler@users.noreply.github.com>
2018-11-20 11:44:44 +0800

    随便2

--- src/components/Home/Home.vue
+++ src/components/Home/Home.vue
@@ -1,7 +1,7 @@
 <template>
 	<div class='home'>
 		<Header></Header>
-    <Nav></Nav>
+    
   </div>
 </template>
 
@@ -9,11 +9,11 @@
 	// https://m.maizuo.com/v4/api/film/coming-soon?__t=1542266404759&page=1&count=3
 	                    // /api/v4/api/film/now-playing'
 import Header from './commons/Header.vue'
-import Nav from './commons/Nav.vue'
+// import Nav from './commons/Nav.vue'
 
 export default{
 	name:'Home',
-	components:{Header,Nav},
+	components:{Header},
     data(){
       return ;
     	
